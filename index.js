14f5d967f3fcd9529ad322587dd65a01ad695e90
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2024-12-17 11:55:33 +0600

    Added jwt token

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 require("dotenv").config();
 const port = process.env.PORT || 3000
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { application } = require('express');
+
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token
+
+  if(!token){
+   return res.status(401).send({message: "Unauthorized Access!"});
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if(error){
+     return res.status(401).send({ message: "Unauthorized Access!" });
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zt90y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,6 +52,18 @@ async function run() {
 
     const jobCollection = client.db("Job-Portal-DB").collection("jobs");
     const applicationCollection = client.db("Job-Portal-DB").collection("applications");
+
+    // Auth related apis
+    app.post("/jwt", async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "1h"});
+      res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
 
     // Job related apis
     app.get("/jobs", async(req, res) => {
@@ -69,9 +104,14 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/application", async(req, res) => {
+    app.get("/application", verifyToken, async(req, res) => {
         const email = req.query.email;
         const query = {applicant_email : email};
+
+        if(req.user.email !== email){
+          return res.status(403).send({message: "Forbidden!"})
+        }
+
         const result = await applicationCollection.find(query).toArray();
 
         for(const application of result){
